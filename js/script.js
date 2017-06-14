c57328c935f49e4cae7fe2bc581558601bab5a60
BretP <bret.prins@gmail.com>
2017-06-14 12:53:10 -0600

    Testing JSON requests with New York times

--- js/script.js
+++ js/script.js
@@ -1,21 +1,39 @@
-
 function loadData() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+  var $body = $('body');
+  var $wikiElem = $('#wikipedia-links');
+  var $nytHeaderElem = $('#nytimes-header');
+  var $nytElem = $('#nytimes-articles');
+  var $greeting = $('#greeting');
 
-    // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
+  // clear out old data before new request
+  $wikiElem.text("");
+  $nytElem.text("");
 
-    // load streetview
+  // load streetview
 
-    // YOUR CODE GOES HERE!
-    var street = $
-    return false;
+  // YOUR CODE GOES HERE!
+  var streetVal = $('#street').val();
+  var cityVal = $('#city').val();
+  var fullAddress = streetVal + ', ' + cityVal;
+
+  $greeting.text('So you want to live on ' + streetVal + '?');
+
+  var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + fullAddress + '';
+  $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  url += '?' + $.param({
+    'api-key': "0122ecaf55c14881a64e98307423ad88"
+  });
+
+  $.getJSON(url, function(data) {
+    console.log(data);
+    var item = data.response.docs;
+    console.log(item);
+  });
+
+  return false;
 };
 
 $('#form-container').submit(loadData);
